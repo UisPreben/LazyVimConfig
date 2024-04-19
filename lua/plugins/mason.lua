f90ad38f50b5ac250ed116a663374633b46c4d80
UiSPreben <p.paulsen@stud.uis.no>
2024-04-19 07:53:54 +0200

    13

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,31 @@
+return {
+  "williamboman/mason.nvim",
+  -- opts will be merged with the parent spec
+  opts = {
+    ensure_installed = {
+      "autopep8",
+      "bash-language-server",
+      "black",
+      "clang-format",
+      "clangd",
+      "glint",
+      "goimports",
+      "golangci-lint",
+      "golangci-lint-langserver",
+      "golines",
+      "gopls",
+      "gotests",
+      "isort",
+      "jedi-language-server",
+      "json-lsp",
+      "lua-language-server",
+      "prettier",
+      "pyright",
+      "python-lsp-server",
+      "rust-analyzer",
+      "shellcheck",
+      "shfmt",
+      "stylua",
+    },
+  },
+}
