a94ecb5344d3525fcd876e50c0977ebcf9e5996d
preben <km8preben@gmail.com>
2024-07-15 23:15:22 +0200

    hi

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,6 @@
 -- Add any additional options here
 vim.opt.colorcolumn = "80"
 vim.opt.scrolloff = 10
+-- vim.g.lazyvim_python_lsp = "basedpyright"
+vim.g.lazyvim_python_lsp = "pyright"
+vim.g.lazyvim_pyton_ruff = "ruff_lsp"
