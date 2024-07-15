1699a20cfdb384bc48be3437805b1aefd35be0c1
preben <km8preben@gmail.com>
2024-07-15 23:16:04 +0200

    hi

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- Hyprlang LSP
+-- Hyprlang LSP requires "yay just"
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   pattern = { "*.hl", "hypr*.conf" },
   callback = function(event)
