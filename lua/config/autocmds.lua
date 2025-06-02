38a8a83f3a49efd8d7b9278f081beda95c91e627
preben <km8preben@gmail.com>
2025-06-02 19:07:37 +0200

    hi

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  desc = "Open Neo-tree on startup",
+  callback = function()
+    -- Small delay to ensure plugins are loaded
+    vim.defer_fn(function()
+      require("neo-tree.command").execute({ action = "show" })
+    end, 10)
+  end,
+})
---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      close_if_last_window = true,
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            -- '.git',
+            -- '.DS_Store',
+            -- 'thumbs.db',
+          },
+          never_show = {},
+        },
+      },
+      window = {
+        width = 30, -- default is usually 40
+        -- width = "25%",
+      },
+    },
+  },
+}
