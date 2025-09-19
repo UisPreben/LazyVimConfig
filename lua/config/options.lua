6200a46ddad8fdd42e8309818d473d544859ee1b
preben <km8preben@gmail.com>
2025-09-19 04:52:22 +0200

    hi

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,8 +3,8 @@
 -- Add any additional options here
 vim.opt.colorcolumn = "80"
 vim.opt.scrolloff = 10
--- vim.g.lazyvim_python_lsp = "basedpyright"
+vim.g.lazyvim_python_lsp = "basedpyright"
 -- vim.g.lazyvim_python_lsp = "jedi_language_server"
-vim.g.lazyvim_python_lsp = "pyright"
+-- vim.g.lazyvim_python_lsp = "pyright"
 -- vim.g.lazyvim_python_lsp = "pylsp"
 vim.g.lazyvim_pyton_ruff = "ruff_lsp"
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,5 @@
 return {
-  "williamboman/mason.nvim",
+  "mason-org/mason.nvim",
   -- opts will be merged with the parent spec
   opts = {
     ensure_installed = {
